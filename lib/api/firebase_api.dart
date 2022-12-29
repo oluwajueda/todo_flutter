c2e40ca9aa41a959f226a16c33cf0ee34f1dc22d
oluwajueda <oluwajuedaloade@gmail.com>
2022-12-29 02:25:48 -0800

    floatingbutton added and todo widget added

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,5 +1,7 @@
+//@dart=2.9
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todo_app_flutter/model/todo.dart';
+import 'package:todo_app_flutter/utils.dart';
 
 class FirebaseApi {
   static Future<String> createTodo(Todo todo) async {
@@ -11,6 +13,12 @@ class FirebaseApi {
 
     return docTodo.id;
   }
+
+  static Stream<List<Todo>> readTodos() => FirebaseFirestore.instance
+      .collection('todo')
+      .orderBy(TodoField.createdTime, descending: true)
+      .snapshots()
+      .transform(Utils.transformer(Todo.fromJson));
 
   static Future deleteTodo(Todo todo) async {
     final docTodo = FirebaseFirestore.instance.collection('todo').doc(todo.id);
--- lib/page/homePage.dart
+++ lib/page/homePage.dart
@@ -1,14 +1,17 @@
+//@dart=2.9
+
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:todo_app_flutter/api/firebase_api.dart';
 import 'package:todo_app_flutter/model/todo.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app_flutter/provider/todos.dart';
+import 'package:todo_app_flutter/widget/add_todo_dialog_widget.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -23,52 +26,65 @@ class _HomePageState extends State<HomePage> {
     ];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('ToDo'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          unselectedItemColor: Colors.white.withOpacity(0.7),
-          selectedItemColor: Colors.white,
-          currentIndex: selectedIndex,
-          onTap: (index) => setState(() {
-            selectedIndex = index;
-          }),
-          // ignore: prefer_const_literals_to_create_immutables
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.fact_check_outlined),
-              label: 'Todos',
+      appBar: AppBar(
+        title: Text('ToDo'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white.withOpacity(0.7),
+        selectedItemColor: Colors.white,
+        currentIndex: selectedIndex,
+        onTap: (index) => setState(() {
+          selectedIndex = index;
+        }),
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fact_check_outlined),
+            label: 'Todos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.done,
+              size: 28,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.done,
-                size: 28,
-              ),
-              label: "completed",
-            )
-          ],
+            label: "completed",
+          )
+        ],
+      ),
+      body: StreamBuilder<List<Todo>>(
+        stream: FirebaseApi.readTodos(),
+        builder: ((context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            default:
+              if (snapshot.hasError) {
+                return buildText('Something Went Wrong Try later');
+              } else {
+                final todos = snapshot.data;
+
+                final provider = Provider.of<TodosProvider>(context);
+                provider.setTodos(todos);
+
+                return tabs[selectedIndex];
+              }
+          }
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
         ),
-        body: StreamBuilder<List<Todo>>(
-          stream: FirebaseApi.readTodos(),
-          builder: ((context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return Center(child: CircularProgressIndicator());
-              default:
-                if (snapshot.hasError) {
-                  return buildText('Something Went Wrong Try later');
-                } else {
-                  final todos = snapshot.data;
-
-                  final provider = Provider.of<TodosProvider>(context);
-                  provider.setTodos(todos);
-
-                  return tabs[selectedIndex];
-                }
-            }
-          }),
-        ));
+        backgroundColor: Colors.black,
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AddTodoDialogWidget(),
+          barrierDismissible: false,
+        ),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 
   Widget buildText(String text) => Center(
